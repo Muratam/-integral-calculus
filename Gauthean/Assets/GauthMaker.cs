e6324a9e82868aaf026aa452599e9ffb59705b8b
CHY72 <c4_diamondia@yahoo.co.jp>
2015-07-12 05:00:54 +0900

    firstcommit

---
+++ Gauthean/Assets/GauthMaker.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GauthMaker : MonoBehaviour {
+    //目的の変換関数
+    public Func<float, float, Vector3> f = ToSphere;
+
+    private static Func<float, float> Sin = Mathf.Sin;
+    private static Func<float, float> Cos = Mathf.Cos;
+
+    
+    public static Vector3 ToMebius(float u, float v) {
+        float a = 2f;
+        return new Vector3(
+            (a + u * Sin(v/2))* Cos(v),
+            u * Sin(v / 2),
+            (a + u * Cos(v/2))* Sin(v)
+            );
+    }
+
+    public static Func<float, float, Vector3> ToDouble
+       = (u, v) => new Vector3(u * 2 , 1, v * 2);
+
+
+    public static Func<float, float, Vector3> ToColumn
+        = (u, v) => new Vector3(Sin(u) + 3.14f, Cos(u), v);
+
+    public static Func<float, float, Vector3> ToSphere
+        = (u, v) => new Vector3(Sin(u) * Cos(v), Cos(u), Sin(u) * Sin(v));
+
+
+
+
+
+    public float UMin = -1, UMax = 1, VMin = -1, VMax = 1;
+    public float MoveTime = 2f;
+    public float UResolution = 0.1f,VResolution = 0.1f;//分解能
+    public bool MakeRoot = true;
+    public bool DrawDebugLine = false;
+
+    public GameObject DefaultQuad;
+    private MoveMesh[,] Meshes;
+
+
+    void Awake() {
+        CreateDefaultMesh();
+    }
+
+    void CreateDefaultMesh() {
+        DefaultQuad.transform.localScale = new Vector3(UResolution, 1, VResolution);
+        int UNum = (int)((UMax - UMin) / UResolution);
+        int VNum = (int)((VMax - VMin) / VResolution);
+        Meshes = new MoveMesh[UNum + 1,VNum + 1];
+
+        for (int k = 0; k < (MakeRoot ? 2 : 1); k++) {
+            for (int i = 0; i < UNum; i++) {
+                for (int j = 0; j < VNum; j++) {
+                    Meshes[i, j] = ((GameObject)(Instantiate(DefaultQuad, new Vector3(UMin + UResolution * (i + 0.5f), 0, VMin + VResolution * (j + 0.5f)), new Quaternion())
+                                    )).GetComponent<MoveMesh>();
+                    Meshes[i, j].GM = this;
+                    Meshes[i, j].MyColor =  ColorHSV.FromHsv((int)(360f * (float)i / (float)UNum), (float)j / (float)VNum, 0.9f);
+                    var Mats = Meshes[i, j].gameObject.GetComponentsInChildren<MeshRenderer>();
+                    foreach (var m in Mats) {
+                        switch (m.gameObject.name) { 
+                            default://case "Quad1":
+                                m.material.color = Meshes[i, j].MyColor;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void Start() {
+        foreach (var m in Meshes) {
+            if (m != null) {
+                m.Init(f, UResolution, VResolution);
+            }
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            foreach (var m in Meshes) {
+                if (m != null) {
+                    m.StartMoving(f, m.GetNormalVector(f,UResolution,VResolution));
+                }
+            }
+        }else if (Input.GetKeyDown(KeyCode.Backspace)) {
+            foreach (var m in Meshes) {
+                if (m != null) {
+                    m.SwapMoving();
+                }
+            }
+        } else if (Input.GetKeyDown(KeyCode.L)) {
+            DrawDebugLine = !DrawDebugLine;
+        
+        }
+    }
+
+
+
+}
---
+++ Gauthean/Assets/MoveMesh.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+public class MoveMesh : MonoBehaviour {
+
+    public GauthMaker GM;
+    public float MoveTime {
+        get{return GM.MoveTime;}
+    }
+    public bool DrawDebugLine {
+        get { return GM.DrawDebugLine; }
+    }
+    private bool startSwitch = false;
+    private bool MoveFinish = false;
+    private float StartTime;
+
+    private Vector3 fromPosition;
+    //最初は(0,1,0)の向きのQuadと仮定
+    private Vector3 fromUp;
+    private Vector3 fromScale;
+
+    private Vector3 toPosition;
+    private Vector3 toUp;
+    private Vector3 ToUp {//法線ベクトルで指示
+        get { return toUp; }
+        set {
+            toUp = value;
+            toUp.Normalize();
+
+        }
+    }
+    private Vector3 toScale;
+
+    public Color MyColor;
+
+    //法線ベクトルを取得
+    public Vector3 GetNormalVector(Func<float, float, Vector3> f,float uResolution,float vResolution) {
+        float u = fromPosition.x;
+        float v = fromPosition.z;
+        Vector3 dU = f(u + uResolution, v) - f(u, v);
+        Vector3 dV = f(u, v + vResolution) - f(u, v);
+        return Vector3.Cross(dU, dV);
+    }
+
+    public Vector2 GetRelativeScale(Func<float, float, Vector3> f, float uResolution, float vResolution) {
+        float u = fromPosition.x;
+        float v = fromPosition.z;
+        Vector3 dU = f(u + uResolution, v) - f(u, v);
+        Vector3 dV = f(u, v + vResolution) - f(u, v);
+        return new Vector2(dU.magnitude / uResolution ,dV.magnitude/vResolution  );
+    }
+
+
+
+    static float AxisAngleOnAxisPlane( Vector3 origin, Vector3 fromDirection, Vector3 toDirection, Vector3 axis ) {
+	    fromDirection.Normalize();
+	    axis.Normalize();
+	    Vector3 toDirectionProjected = toDirection - axis * Vector3.Dot(axis,toDirection);
+	    toDirectionProjected.Normalize();
+	    return Mathf.Acos(Mathf.Clamp(Vector3.Dot(fromDirection,toDirectionProjected),-1f,1f)) *
+		    (Vector3.Dot(Vector3.Cross(axis,fromDirection), toDirectionProjected) < 0f ? -Mathf.Rad2Deg : Mathf.Rad2Deg);
+    }
+
+
+
+    
+    public void Init(Func<float, float, Vector3> f,float uResolution,float vResolution) {
+        fromPosition = this.transform.position;
+        fromScale = this.transform.localScale;
+        fromUp = this.transform.up;
+        toPosition = f(fromPosition.x, fromPosition.z);
+        toUp = GetNormalVector(f,uResolution,vResolution);
+        Vector2 RelativeScale = GetRelativeScale(f,uResolution,vResolution);
+        toScale = new Vector3(fromScale.x * RelativeScale.x, 1,fromScale.z * RelativeScale.y);
+    }
+
+    public void StartMoving(Func<float,float,Vector3> f,Vector3 _ToUp) {
+        startSwitch = true;
+        StartTime = Time.time;
+        MoveFinish = false;
+    }
+
+    public void SwapMoving() {
+        if (MoveFinish) {
+            Swap(ref toPosition, ref fromPosition);
+            Swap(ref toUp, ref fromUp);
+            Swap(ref toScale,ref fromScale);
+            startSwitch = true;
+            MoveFinish = false;
+            StartTime = Time.time;
+        }
+    }
+    static void Swap<T>(ref T V1,ref T V2) {
+        var Temp = V1;V1 = V2; V2 = Temp;
+    }
+
+
+	void Update () {
+        if (startSwitch && !MoveFinish) {
+            float NormalizedPassedTime = (Time.time - StartTime)/MoveTime;
+            transform.position = fromPosition + NormalizedPassedTime * (toPosition - fromPosition);
+            transform.up = fromUp + NormalizedPassedTime * (ToUp - fromUp);
+            transform.localScale = fromScale + NormalizedPassedTime * (toScale - fromScale);
+            if (NormalizedPassedTime > 1) {
+                transform.position = toPosition;
+                transform.up = ToUp;
+                transform.localScale = toScale;
+                MoveFinish = true;
+            }
+        }
+
+        if(DrawDebugLine)Debug.DrawLine(fromPosition, toPosition,MyColor);
+	}
+}
